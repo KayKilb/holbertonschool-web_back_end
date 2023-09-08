b8af8f8f5fc35dd3bc5a6e9c773fb0788b4a267a
KayKilb <infulbloom@msn.com>
2023-09-08 02:39:50 +0000

    1-12 lint

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,13 +2,13 @@
 // modifying variables
 /* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
   }
+
+  return [task, task2];
+}
