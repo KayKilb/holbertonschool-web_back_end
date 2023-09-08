eb633402b7e5160fa4180ea3b3873fda5c25a934
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-09-07 21:15:20 -0500

    loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+    for (const value of array) {
+      const idx = array.indexOf(value);
+      array[idx] = appendString + value;
+    }
+    return array;
+  }
