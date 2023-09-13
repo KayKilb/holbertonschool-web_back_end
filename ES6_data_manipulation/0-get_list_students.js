e2b9cceb48e1d8105c8ac9b4ba4ecc5c4002d9ae
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-09-12 22:52:44 -0500

    fixed syntax error

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,9 @@
-function getListStudentIds(arr){
-    // Check if the input is an array
-    if (!Array.isArray(arr)) {
-      // If not, return an empty array
-      return [];
-    }
-
-    // Return the array of student IDs
-    return arr.map((obj) => obj.id);
+function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
   }
-  
-  export default getListStudentIds;
+
+  return arr.map((obj) => obj.id);
+}
+
+export default getListStudentIds;
