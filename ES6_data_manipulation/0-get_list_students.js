de75c7f2fd52f9898dde42d7c03d4494f5d5240f
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-09-13 09:40:31 -0500

    file 0 was not working, updated func call

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
-function getListStudentIds(arr) {
-  if (!Array.isArray(arr)) {
-    return [];
-  }
-
-  return arr.map((obj) => obj.id);
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
 
-export default getListStudentIds;
+export default getListStudents;
